c18f3d2948ec49f55e8d754dc71d130779a226e8
Laura <lauramdrsa@gmail.com>
2025-05-31 16:51:59 -0300

    exercícios

---
+++ Apostila introdução Python/DicionárioEx.py
@@ -0,0 +1,7 @@
+#Lanchonete
+cardapio = {"Salgado":4.50,
+            "Lanche":6.50,
+            "Suco": 3.00,
+            "Refrigerante":3.50,
+            "Brigadeiro": 1.00}
+print (cardapio)
---
+++ Apostila introdução Python/ExCardapio.py
@@ -0,0 +1,10 @@
+card = (4.50,6.50,3.00,3.50,1.00)
+sal,lan,suc,ref,bri = card
+
+print("Cardápio")
+print("Salgado - R$",sal)
+print("Lanche - R$",lan)
+print("Suco - R$",suc)
+print("Refrigerante - R$",ref)
+print("Brigadeiro - R$",bri)
+
