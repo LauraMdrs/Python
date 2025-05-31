7abfe11aa8309f062e32246123871ed4c02fa0df
Laura <lauramdrsa@gmail.com>
2025-05-31 15:16:17 -0300

    exercícios

---
+++ ListaExer.py
@@ -0,0 +1,13 @@
+# Exercício com Range:
+
+l = [5,7,2,9,4,1,3]
+
+print ("Lista = ", l)
+print ("O tamanho da lista é ", len(l))
+print ("O menor número da lista é ", min(l))
+print ("O maior número da lista é ", max(l))
+print ("A soma de todos os números da lista da ", sum(l))
+l.sort()
+print ("Lista em ordem crescente: ", l)
+l.reverse()
+print ("Lista em ordem decrescente: ",l)
---
+++ Range.py
@@ -0,0 +1,10 @@
+# Exemplos de Ranges:
+
+l1 = list(range(11))
+print(l1)
+
+l2 = list(range(10,21))
+print(l2)
+
+l3 = list(range(0,21,5))
+print(l3)
